d53ae1f4e2f471f88453aca3c0701a4a1203ae01
saikumark <kolichelimisaikumar9@gmail.com>
2022-08-23 07:45:50 +0530

    index

---
+++ script.js
@@ -0,0 +1,8 @@
+// document.querySelector("h3").style.backgroundColor = "red";
+
+
+const nodeList = document.querySelectorAll("div > p");
+        for (let i = 0; i < nodeList.length; i++) {
+          nodeList[i].style.backgroundColor = "Green";
+        }
+       
